9d3394d777e9eb9dc8791fb04ed2caa4f92386bf
sinanm89 <sinanm89@gmail.com>
2018-09-10 16:53:45 -0400

    we need to take recursion limit into considretion

--- escape_eschelon.py
+++ escape_eschelon.py
@@ -1,4 +1,7 @@
+from copy import copy
 import json
+
+PYTHON_MAX_RECURSION_LIMIT = 1000
 
 inp = {
     "asteroids": [
@@ -37,45 +40,11 @@ inp = {
 #             "t_per_asteroid_cycle": 4
 #         },
 #         {
-#             "offset": 4,
+#             "offset": 1,
 #             "t_per_asteroid_cycle": 2
 #         }
 #     ]
 # }
-
-
-def merge(left, right):
-    out = []
-    i, j = 0, 0
-    while i < len(left) and j < len(right):
-        l_check = left[i]['offset']
-        r_check = right[j]['offset']
-        if l_check > r_check:
-            out.append(right[j])
-            j += 1
-        else:
-            out.append(left[i])
-            i += 1
-    out += left[i:]
-    out += right[j:]
-    return out
-
-
-def merge_sort(arr):
-    # pythons internal .sort does timsort which is similar to this,
-    # their avg complexity is both nlogn.
-
-    # // floors float val to int
-    mid = len(arr) // 2
-    if len(arr) <= 2:
-        l_check = arr[0].get('offset')
-        r_check = arr[-1].get('offset')
-
-        # last element check doesnt throw index error when len(arr)==1
-        if l_check > r_check:
-            arr[-1], arr[0] = arr[0], arr[-1]
-        return arr
-    return merge(merge_sort(arr[:mid]), merge_sort(arr[mid:]))
 
 
 class Ship(object):
@@ -87,25 +56,16 @@ class Ship(object):
     blast_position = 0
 
     solution = []
+    visited = []
+
+    def __repr__(self):
+        return "<Ship at {0} with {1} v>".format(self.pos, self.v)
 
     def __init__(self, data):
-        self.asteroids = merge_sort(data['asteroids'])
+        self.asteroids = data['asteroids']
         self.t_per_blast_move = data.get('t_per_blast_move')
-        self.max_offset = self.asteroids[-1].get('offset')
-        print("MAX OFFSET IS : {0}".format(self.max_offset))
-        self.asteroid_map = {}
-        # map asteroids by offset/position
-        for i in self.asteroids:
-            if not self.asteroid_map.get(i.get('offset'), False):
-                self.asteroid_map[i.get('offset')] = []
-            self.asteroid_map[i.get('offset')].append(i)
-
-    def potential_positions(self):
-        # the highest weighted diretion is accelating forwards
-        # since were getting closer to the safezone
-        directions = [1, 0, -1]
-        # position_movement_weight = positions_offset + v + a + d
-        return [(self.pos + self.v + d) for d in directions]
+        self.safety_distance = len(self.asteroids)
+        print("SAFETY IS AT : {0}".format(self.safety_distance))
 
     def blast_zone_check(self):
         if (
@@ -120,43 +80,104 @@ class Ship(object):
             self.blast_position += self.t_per_blast_move
         return True
 
-    def is_offset_full_next_turn(self, offset):
+    def potential_positions(self):
+        # the highest weighted diretion is accelating forwards
+        # since were getting closer to the safezone
+        directions = [1, 0, -1]
+        # position_movement_weight = positions_offset + v + a + d
+        out = []
+        for d in directions:
+            next_pos = self.pos + self.v + d
+            if next_pos in self.visited:
+                continue
+            if next_pos >= len(self.asteroids):
+                return ['end', d]
+            if not self.pos_full_next_turn(next_pos):
+                out.append((self.pos + self.v + d))
+        return out
+
+    def pos_full_next_turn(self, pos):
         # get the astroids of offset
-        asteroids = self.asteroid_map.get(offset, [])
-        if len(asteroids) < 1:
-            return False
-        for ast in asteroids:
-            if (self.t + 1) % ast.get('t_per_asteroid_cycle') == 0:
-                return True
+        ast = self.asteroids[pos - 1]
+        cycle = ast.get('t_per_asteroid_cycle')
+        if (ast.get('offset') + self.t + 1) % cycle == 0:
+            return True
         return False
 
-    def move(self):
-        if self.pos >= self.max_offset:
-            return self.solution
+    def try_to_move(self):
+        queue = self.potential_positions()
+
+        if self.pos >= self.safety_distance:
+            return True
+        # if in blast
         if not self.blast_zone_check():
             return False
-        maybe_move = self.potential_positions()
-        while maybe_move:
-            next_pos = maybe_move.pop(0)
-            print('-'*90); import pdb; pdb.set_trace()  # breakpoint 91ff27e1  noqa  //
 
-            if not self.is_offset_full_next_turn(next_pos):
-                # update ship values
-                # move 1 t
-                self.a = next_pos - (self.pos + self.v)
-                print("t: {0} p: {1} v: {2} a: {3}".format(
-                    self.t, self.pos, self.v, self.a))
-                self.v += self.a
-                self.pos = next_pos
-                self.t += 1
-                self.solution.append(self.a)
-                return self.move()
-        return False
+        if len(queue) == 0:
+
+            self.solution.pop()
+            return self.parent.try_to_move()
+
+        if queue[0] == 'end':
+            self.solution.append(queue[-1])
+            print(self.solution)
+            return True
+        return self.move(queue)
+
+    def move(self, queue):
+        for next_pos in queue:
+            # not full and not visited node
+            # if next_pos == 'end':
+            #     self.solution.append(queue[-1])
+            #     print(self.solution)
+            #     return True
+            child = copy(self)
+            child.visited = []
+            child.t += 1
+            child.pos = next_pos
+            child.v = next_pos - self.pos
+            d = next_pos - self.pos - self.v
+            self.solution.append(d)
+
+            child.parent = self
+            child.parent.visited.append(next_pos)
+
+            return child.try_to_move()
+    # def move(self):
+    #     if self.pos >= self.max_offset:
+    #         return self.solution
+    #     if not self.blast_zone_check():
+    #         return False
+
+    #     maybe_move = self.potential_positions()
+    #     print("t: {0} p: {1} v: {2} a: {3}\nvisited:{4} ".format(
+    #         self.t, self.pos, self.v, maybe_move, self.visited))
+
+    #     self.parent = copy(self)
+    #     for next_pos in maybe_move:
+    #         if not self.is_pos_full_next_turn(next_pos):
+    #             a = next_pos - (self.pos + self.v)
+    #             # print("t: {0} p: {1} v: {2} a: {3}".format(
+    #             #     self.t, self.pos, self.v, a))
+    #             self.v += a
+    #             self.pos = next_pos
+    #             self.t += 1
+    #             self.solution.append(a)
+    #             self.parent.visited.append(next_pos)
+    #             return self.move()
+
+    #     self.parent.t -= 1
+    #     self.parent.visited.append(self.pos)
+    #     self.parent.pos = self.parent.pos - self.parent.v
+    #     self.parent.v = self.parent.v - self.parent.solution.pop()
+
+    #     return self.parent.move()
+
 
 with open("v3_chart.json") as f:
     data = f.read()
 
 inp = json.loads(data)
 ship = Ship(inp)
-ship.move()
-print(ship.solution)
+ee = ship.try_to_move()
+print(ee)
