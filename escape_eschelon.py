f104245631b2cfb7457ad36a10cdac83ddc96968
sinanm89 <sinanm89@gmail.com>
2018-09-10 04:26:30 -0400

    pre-misunderstanding

--- escape_eschelon.py
+++ escape_eschelon.py
@@ -1,8 +1,4 @@
-# t_per_asteroid_cycle = time it takes to make one full cycle
-
-# if v > 1 , cant hit asteroid if new space is empty.
-
-# - sort by offset
+import json
 
 inp = {
     "asteroids": [
@@ -11,16 +7,41 @@ inp = {
             "offset": 0
         },
         {
-            "t_per_asteroid_cycle": 2,
+            "t_per_asteroid_cycle": 3,
             "offset": 1
         },
         {
-            "t_per_asteroid_cycle": 1,
-            "offset": 2
+            "t_per_asteroid_cycle": 2,
+            "offset": 4
+        },
+        {
+            "t_per_asteroid_cycle": 2,
+            "offset": 3
         },
     ],
     "t_per_blast_move": 2
 }
+# inp = {
+#     "t_per_blast_move": 10,
+#     "asteroids": [
+#         {
+#             "offset": 0,
+#             "t_per_asteroid_cycle": 2
+#         },
+#         {
+#             "offset": 1,
+#             "t_per_asteroid_cycle": 3
+#         },
+#         {
+#             "offset": 3,
+#             "t_per_asteroid_cycle": 4
+#         },
+#         {
+#             "offset": 4,
+#             "t_per_asteroid_cycle": 2
+#         }
+#     ]
+# }
 
 
 def merge(left, right):
@@ -32,7 +53,7 @@ def merge(left, right):
         if l_check > r_check:
             out.append(right[j])
             j += 1
-        elif l_check < r_check:
+        else:
             out.append(left[i])
             i += 1
     out += left[i:]
@@ -41,6 +62,9 @@ def merge(left, right):
 
 
 def merge_sort(arr):
+    # pythons internal .sort does timsort which is similar to this,
+    # their avg complexity is both nlogn.
+
     # // floors float val to int
     mid = len(arr) // 2
     if len(arr) <= 2:
@@ -60,77 +84,79 @@ class Ship(object):
     a = 0
     pos = 0
     t = 0
+    blast_position = 0
 
-    def __init__(self, asteroids):
-        self.asteroids = merge_sort(inp['asteroids'])
-        max_offset = self.asteroids[-1].get('offset')
-        asteroid_map = {}
+    solution = []
+
+    def __init__(self, data):
+        self.asteroids = merge_sort(data['asteroids'])
+        self.t_per_blast_move = data.get('t_per_blast_move')
+        self.max_offset = self.asteroids[-1].get('offset')
+        print("MAX OFFSET IS : {0}".format(self.max_offset))
+        self.asteroid_map = {}
         # map asteroids by offset/position
         for i in self.asteroids:
-            asteroid_map[i.get('offset')] = asteroid_map.get(i.get('offset'), [])
-            asteroid_map[i.get('offset')].append(i.get('offset'))
-
-    def acc(self):
-        self.a += 1
-        return self.v + self.a
-
-    def dec(self):
-        self.a += 1
-        return self.v - self.a
-
-    def is_offset_full(t, offset):
-        asteroids = self.asteroid_map.get(offset)
-        for ast in asteroids:
-            one_cycle = ast.get('t_per_asteroid_cycle')
-            if t % one_cycle == 0:
-                return True
-        return False
+            if not self.asteroid_map.get(i.get('offset'), False):
+                self.asteroid_map[i.get('offset')] = []
+            self.asteroid_map[i.get('offset')].append(i)
 
     def potential_positions(self):
         # the highest weighted diretion is accelating forwards
         # since were getting closer to the safezone
         directions = [1, 0, -1]
-        return [(self.pos + self.v + self.a + d) for d in directions]
+        # position_movement_weight = positions_offset + v + a + d
+        return [(self.pos + self.v + d) for d in directions]
+
+    def blast_zone_check(self):
+        if (
+            self.pos <= self.blast_position and
+            self.blast_position != 0 and
+            self.pos < 0
+        ):
+            print('blown to bits at pos: {0} and blast pos :{1}'.format(
+                self.pos, self.blast_position))
+            return False
+        if self.t > 0 and self.t % self.t_per_blast_move == 0:
+            self.blast_position += self.t_per_blast_move
+        return True
+
+    def is_offset_full_next_turn(self, offset):
+        # get the astroids of offset
+        asteroids = self.asteroid_map.get(offset, [])
+        if len(asteroids) < 1:
+            return False
+        for ast in asteroids:
+            if (self.t + 1) % ast.get('t_per_asteroid_cycle') == 0:
+                return True
+        return False
 
     def move(self):
-        # look at possible movements
-        # calculate the overall cost
-        # add all routes to priority queue
-        # choose the smallest one.
-        # keep in mind the direction
+        if self.pos >= self.max_offset:
+            return self.solution
+        if not self.blast_zone_check():
+            return False
+        maybe_move = self.potential_positions()
+        while maybe_move:
+            next_pos = maybe_move.pop(0)
+            print('-'*90); import pdb; pdb.set_trace()  # breakpoint 91ff27e1  noqa  //
 
-        # nlogn complexity
+            if not self.is_offset_full_next_turn(next_pos):
+                # update ship values
+                # move 1 t
+                self.a = next_pos - (self.pos + self.v)
+                print("t: {0} p: {1} v: {2} a: {3}".format(
+                    self.t, self.pos, self.v, self.a))
+                self.v += self.a
+                self.pos = next_pos
+                self.t += 1
+                self.solution.append(self.a)
+                return self.move()
+        return False
 
-        maybe_move = []
+with open("v3_chart.json") as f:
+    data = f.read()
 
-        current_t = 0
-        current_offset_asteroids = []
-        prev_offset = ordered_by_distance_asteroids[0].get('offset')
-
-        blast_position = 0
-
-        def blast_zone(current_t, blast_position):
-            if current_t % t_per_blast_move == 0:
-                blast_position += t_per_blast_move
-            return blast_position
-
-        # position_movement_weight = positions_offset + v + a
-        for i in self.asteroids:
-            next_pos = []
-            if prev_offset != i.get('offset'):
-
-                # we have all the asteroids on pos
-                if is_offset_full(t, current_offset_asteroids):
-                    # check legal move
-                    if maybe_move:
-                        # if we have a legal move where we can go forward or backward
-                    else:
-                        pass
-                        # wait 1 time without changing position for asteroids to pass
-                # calculate the next move costs
-            else:
-                current_offset_asteroids.append(i)
-            prev_offset = i.get('offset')
-
-
+inp = json.loads(data)
 ship = Ship(inp)
+ship.move()
+print(ship.solution)
