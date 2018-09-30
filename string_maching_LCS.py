ab4b960021e1c3914a2ebfc98ae9934b7ce846e4
sinanm89 <sinanm89@gmail.com>
2018-09-30 00:36:25 -0400

    works and explanation added

--- string_maching_LCS.py
+++ string_maching_LCS.py
@@ -1,49 +1,61 @@
 def LCSLength(X, Y):
+    X = "_" + X
+    Y = "_" + Y
+    # print(X , Y)
     m, k = len(X), len(Y)
     C = [[0 for q in range(0, k)] for r in range(0, m)]
     B = [['-' for q in range(0, k)] for r in range(0, m)]
     for i in range(1, m):
         for f in range(1, k):
             if X[i] == Y[f]:
-                # try:
                 C[i][f] = C[i-1][f-1] + 1
-                B[i][f] = u'\u2196'
-                # except:
-                    # import pdb; pdb.set_trace()
+                B[i][f] = 'd'
             elif C[i-1][f] >= C[i][f-1]:
                 C[i][f] = C[i-1][f]
-                B[i][f] = u'\u2191'
+                B[i][f] = 'u'
 
             else:
                 C[i][f] = max(C[i][f-1], C[i-1][f])
                 B[i][f] = '-'
     return C, B
 
+# def backtrackAll(C, X, Y, i, j):
+#     if i == 0 or j == 0:
+#         return {""}
+#     if X[i] == Y[j]:
+#         return {Z + X[i] for Z in backtrackAll(C, X, Y, i-1, j-1)}
+#     R = {""}
+#     if C[i][j-1] >= C[i-1][j]:
+#         # import pdb; pdb.set_trace()
+#         R = R.union(backtrackAll(C, X, Y, i, j-1))
+#     if C[i-1][j] >= C[i][j-1]:
+#         # import pdb; pdb.set_trace()
+#         R = R.union(backtrackAll(C, X, Y, i-1, j))
+#     return R
 
-def backtrackAll(C, X, Y, i, j):
-    if i == 0 or j == 0:
-        return {""}
-    if X[i] == Y[j]:
-        return {Z + X[i] for Z in backtrackAll(C, X, Y, i-1, j-1)}
-    R = {""}
-    if C[i][j-1] >= C[i-1][j]:
-        # import pdb; pdb.set_trace()
-        R = R.union(backtrackAll(C, X, Y, i, j-1))
-    if C[i-1][j] >= C[i][j-1]:
-        # import pdb; pdb.set_trace()
-        R = R.union(backtrackAll(C, X, Y, i-1, j))
-    return R
+def backtrackLCS(word, solution_t, x, y):
+    if x == 0 or y == 0:
+        return 
+    if solution_t[x][y] == 'd':
+        backtrackLCS(word, solution_t, x-1, y-1)
+        # print(word[y-1]) # w2
+        print(word[x-1]) # w1
+    elif solution_t[x][y] == 'u':
+        backtrackLCS(word, solution_t, x-1, y)
+    else:
+        backtrackLCS(word, solution_t, x, y-1)
 
-# match the longest common string in the given strings.
+# match the longest common string in the given strings by taking
+# out 0 or more elements from either string
 w1, w2 = list("ABCDEFG"), list("BCDGK")
 # MJAU
 w1, w2 = "XMJYAUZ", "MZJAWXU"
-# w1, w2 = "SHINCHAN", "NOHARAAA"
-
-w1,w2 = "GAC","AGCAT"
-w1,w2 = "_ABCBDAB","-BDCABA"
+w1, w2 = "SHINCHAN", "NOHARAAA"
+# w1,w2 = "GAC","AGCAT"
+# w1,w2 = "ABCBDAB","BDCABA"
 C, B = LCSLength(w1, w2)
 for i in range(len(C)): print(C[i], "\t", B[i]) 
 print('-'*30)
-
-print(backtrackAll(C, w1, w2, len(w1)-1, len(w2)-1))
+# print(backtrackAll(C, w1, w2, len(w1)-1, len(w2)-1))
+import pdb; pdb.set_trace()
+backtrackLCS(w1, B, len(B)-1, len(B[0])-1)
