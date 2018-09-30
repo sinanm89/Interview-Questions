21a75c98ff69cfa6a55dbe83df71f71df910385c
sinanm89 <sinanm89@gmail.com>
2018-09-30 01:41:27 -0400

    optimised lcs

--- string_maching_LCS.py
+++ string_maching_LCS.py
@@ -1,23 +1,25 @@
-def LCSLength(X, Y):
+def optimised_LCSLength(X, Y):
     X = "_" + X
     Y = "_" + Y
-    # print(X , Y)
     m, k = len(X), len(Y)
-    C = [[0 for q in range(0, k)] for r in range(0, m)]
-    B = [['-' for q in range(0, k)] for r in range(0, m)]
+    prev = [0 for q in range(0, k)]
+    B = [[None for q in range(0, k)] for r in range(0, m)]
     for i in range(1, m):
+        temp = [0 for q in range(0, k)]
         for f in range(1, k):
             if X[i] == Y[f]:
-                C[i][f] = C[i-1][f-1] + 1
-                B[i][f] = 'd'
-            elif C[i-1][f] >= C[i][f-1]:
-                C[i][f] = C[i-1][f]
-                B[i][f] = 'u'
+                temp[f] = prev[f-1] + 1
+                B[i][f] = 1
 
+            elif prev[f] >= temp[f-1]:
+                temp[f] = prev[f]
+                B[i][f] = 0
             else:
-                C[i][f] = max(C[i][f-1], C[i-1][f])
-                B[i][f] = '-'
-    return C, B
+                temp[f] = max(temp[f-1], prev[f])
+                B[i][f] = None
+
+        prev = temp
+    return temp[-1], B
 
 # def backtrackAll(C, X, Y, i, j):
 #     if i == 0 or j == 0:
@@ -53,9 +55,9 @@ w1, w2 = "XMJYAUZ", "MZJAWXU"
 w1, w2 = "SHINCHAN", "NOHARAAA"
 # w1,w2 = "GAC","AGCAT"
 # w1,w2 = "ABCBDAB","BDCABA"
-C, B = LCSLength(w1, w2)
-for i in range(len(C)): print(C[i], "\t", B[i]) 
+max_len, solution = optimised_LCSLength(w1,w2)
+for i in range(len(C)): print(C[i], "\t", B[i])
 print('-'*30)
 # print(backtrackAll(C, w1, w2, len(w1)-1, len(w2)-1))
 import pdb; pdb.set_trace()
-backtrackLCS(w1, B, len(B)-1, len(B[0])-1)
+backtrackLCS(w1, solution, len(solution)-1, len(solution[0])-1)
