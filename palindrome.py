a65456fde405fac752136b1c3bf39f9eb8bf8ed0
sinanm89 <sinanm89@gmail.com>
2018-10-11 17:27:34 -0400

    commit new single pass palindrome finder

--- palindrome.py
+++ palindrome.py
@@ -1,134 +1,84 @@
+def spalin2(n, inp):
+    """
+    Single pass special palindrome calculator.
+    aaabaaa = correct
 
-def find_palindrome(inp):
-    char_map = {}
-    p_mirror = ''
-    out = []
-    temp_i = 0
-
-    for i in range(0, len(inp)):
-        c = inp[i]
-        char_map[c] = char_map.get(c, 0) + 1
-        if char_map[c] % 2 == 0:
-            m_count = 1
-            sp = ''
-            for k in range(0, i):
-                k = i - k - 1
-                if inp[k] != c:
-                    if inp[k-m_count] == c:
-                        sp = inp[k-m_count:i+1]
-                        if len(sp) == 1:
-                            break
-                        out.append(sp)
-                    break
-                else:
-                    m_count += 1
-                    sp = inp[k:i+1]
-                    if len(sp) == 1:
-                        break
-                    out.append(sp)
-            char_map[c] = 1
-
+    aabcbaa = not correct
+    """
+    out = n
+    curr_i = 0
+    while curr_i < n:
+        factorial_count = 0
+        next_i = curr_i + 1
+        if next_i >= n:
+            break
+        if next_i < n and inp[curr_i] == inp[next_i]:
+            # strings like aa aaaa fall here
+            while next_i < n and inp[curr_i] == inp[next_i]:
+                factorial_count += 1
+                next_i += 1
+            # we already count every single letter on exit
+            while factorial_count > 0:
+                # aaaa = 4+3+2+1 = 10
+                # aaaaa = 5+4+3+2+1 = 15
+                out += factorial_count
+                factorial_count -= 1
+            # import pdb; pdb.set_trace()
+            curr_i = next_i
+        else:
+            prev_i = curr_i - 1
+            if prev_i < 0 or prev_i == curr_i:
+                curr_i += 1
+                continue
+            elif (
+                prev_i >= 0 and
+                inp[prev_i] == inp[next_i] and
+                inp[curr_i] != inp[prev_i]
+            ):
+                # import pdb; pdb.set_trace()
+                mirrored_char = inp[next_i]
+                while (
+                    next_i < n and prev_i >= 0 and
+                    inp[prev_i] == inp[next_i] and
+                    inp[prev_i] == mirrored_char
+                ):
+                    # aba aabaa aaabaaa
+                    # the lefthand side of the mirrored string is counted
+                    out += 1
+                    prev_i -= 1
+                    next_i += 1
+            curr_i += 1
     return out
-w1 = 'mnonopoo' # 12
-w2 = 'abcbaba' # 10
-w3 = 'aaaa' # 10
-w4 = 'aabaa' #   aa, aba, aa, aabaa 9
-w5 = 'asasd' #   asa sas 7
-w6 = 'asaa' #   asa aa 6
 
-# out = find_palindrome(w1)
-# print(out, len(out) +  len(w1))
+w1 = 'aabaa' # aa aba aabaa aa + 5 = 9
+w2 = 'asasd' # asa sas + 5 = 7
+w3 = 'abcbaba' # bcb bab aba + 7
+w4 = 'aaaa' # aa aa aa aaa aaa aaaa + 4 = 10
+w6 = 'abcccb'
+w7 = 'baaacbabbaaabaabbaa' # ee = ["aa", "aa", "aaa", "bab", "bb", "aa", "aa", "aaa", "aba", "aabaa", "aa","bb", "aa"]
 
-# out = find_palindrome(w2)
-# print(out, len(out) +  len(w2))
+print(spalin2(len(w1), w1))
 
-# out = find_palindrome(w3)
-# print(out, len(out) +  len(w3))
+print(spalin2(len(w2),w2))
 
-# out = find_palindrome(w4)
-# print(out, len(out) +  len(w4))
+print(spalin2(len(w3),w3))
 
-# out = find_palindrome(w5)
-# print(out, len(out) +  len(w5))
-# import pdb; pdb.set_trace()
-# out = find_palindrome(w6)
-# print(out, len(out) +  len(w6))
-
-# WORKING WEIRD ASS SOLUTION
-# def substrCount(n, s):
-#     l = []
-#     count = 0
-#     cur = None
-
-# # 1st pass
-#     for i in range(n):
-#         if s[i] == cur:
-#             count += 1
-#         else:
-#             if cur is not None:
-#                 l.append((cur, count))
-#             cur = s[i]
-#             count = 1
-#     l.append((cur, count))
-
-#     ans = 0
-        
-# # 2nd pass
-#     for i in l:
-#         ans += (i[1] * (i[1] + 1)) // 2
-
-# # 3rd pass
-#     for i in range(1, len(l) - 1):
-#         if l[i - 1][0] == l[i + 1][0] and l[i][1] == 1:
-#             ans += min(l[i - 1][1], l[i + 1][1])
-
-#     return ans
-
-# print(substrCount(len(w1), w1))
-# print(substrCount(len(w2), w2))
-# print(substrCount(len(w3), w3))
-# print(substrCount(len(w4), w4))
-# print(substrCount(len(w6), w6))
-
-# BETTER SOLUTION
-# def substrCount(n, s):
-#     ret = n
-#     i = 1
-#     while i < n:
-# #First part: find and calculate in substrings that have all same characters
-#         l = i
-#         r = i
-#         while l > 0 and s[l-1] == s[i]:
-#             l -= 1
-#         while r < n-1 and s[r+1] == s[i]:
-#             r += 1        
-#         if r - l != 0:
-#             size = r - l + 1
-#             ret += (size * (size-1)) // 2
-#             i = r + 1
-#             continue
-        
-# #Second part: if current character is different than it's both neighbor,
-# # see if there is any substring contain special palindrome (where this character is the center of substring)
-#         curr = s[i-1]
-#         j = 1
-#         while i-j >= 0 and i+j < n:
-#             if s[i-j] == curr and s[i+j] == curr:
-#                 ret += 1
-#                 j += 1
-#             else:
-#                 break
-#         i += 1
-#     return ret
+print(spalin2(len(w4), w4))
+print(spalin2(len(w6), w6))
+print(spalin2(len(w7), w7))
 
 
+with open('input02.txt') as f:
+    read_data = f.read()
+len_inp = 0
+for i in read_data:
+    if i == '\n':
+        break
+    len_inp += 1
 
-def special_palindrone(inp):
-    out = []
-    for curri in range(1, inp-1):
-        curr = inp[curri]
-        prev = inp[curr-1]
-        nexx = inp[curr+1]
-        if curr != prev and curr != nexx:
-            if prev == nexx:
-                out.append(curr[curri-1:curri+2])
+w5 = read_data[len_inp+1:]
+len_inp = int(read_data[:len_inp])
+print(spalin2(len_inp, w5))
+
+# print(substrCount(len_inp, w5))
+# print(substrCount(len(w7), w7))
